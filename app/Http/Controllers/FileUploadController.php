21cfd5714920f6cc15a011876b23c9b1fd79e2b8
ryohei <leoism999@gmail.com>
2024-09-11 23:02:45 +0900

    add image file upload page

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -14,14 +14,18 @@ class FileUploadController extends Controller
 
     public function upload(Request $request)
     {
-        // バリデーション（ファイルがアップロードされたか、jpg, png形式か確認）
+        // バリデーション
         $request->validate([
-            'file' => 'required|file|mimes:jpg,png,jpeg|max:2048',
+            'file' => 'required|file|mimes:jpg,png,jpeg,gif|max:2048',
         ]);
 
-        // ファイルを保存
-        $path = $request->file('file')->store('public/img');
+        // 元のファイル名を取得
+        $originalName = $request->file('file')->getClientOriginalName();
+
+        // ファイルを保存（元の名前を使って保存）
+        $path = $request->file('file')->storeAs('public/img', $originalName);
 
         return redirect()->back()->with('success', 'ファイルがアップロードされました。');
     }
+
 }
